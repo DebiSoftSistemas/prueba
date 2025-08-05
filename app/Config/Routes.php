b022f4f11d2693f2137a55ef74ef9e99aeff61b9
PAZMIÑO TORRES SERGIO EDWIN <sepazminot@utn.edu.ec>
2025-08-05 16:50:37 -0500

    arreglo de rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,13 +16,9 @@ $routes->get('/vacio', 'Rothman::vacio');
 $routes->get('/bienvenida', 'Servicio\Ejemplo::bienvenida');
 $routes->get('/jsonResponse', 'Servicio\Ejemplo::jsonBienvenida');
 $routes->get('/jsonTexto/(:segment)', 'Servicio\Ejemplo::jsonTexto/$1');
-<<<<<<< HEAD
-$routes->get('/jsonTexto', 'Servicio\Ejemplo::jsonTexto');
-=======
 $routes->get('/jsonTexto', 'Servicio\Ejemplo::jsonTexto');
 
 //
 $routes->post('/','Servicio\spusuario::usuario');
->>>>>>> fcf3659659c4575ff84acce4a702f6afee285271
 
 $routes->post('/', 'Servicio\christian::usercris');

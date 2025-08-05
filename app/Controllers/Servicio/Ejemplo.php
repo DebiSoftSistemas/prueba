29f61b9efd507f351691fb142a2816dd2b9efc82
DebiSoftSistemas <sistemas@debi.ec>
2025-08-05 15:17:25 -0500

    subida de bienvanida

--- app/Controllers/Servicio/Ejemplo.php
+++ app/Controllers/Servicio/Ejemplo.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controllers\Servicio;
 use CodeIgniter\RESTful\ResourceController;
-class Ejemplo extends ResourceController{
+class Ejemplo extends ResourceController {
     protected $model;
 
     public function __construct(){
@@ -9,12 +9,6 @@ class Ejemplo extends ResourceController{
     }
 
     public function bienvenida() {
-        return $this->genericResponse("BIENVENIDO A CODEIGNITER 4");
-    }
-
-    private function genericResponse($data){
-        $this->response->setHeader('Access-Control-Allow-Origin', '*');
-        $this->response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        return $this->respond($data);
+        return $this->respond("BIENVENIDO A CODEIGNITER 4");
     }
 }
